73244c38a65c9d481669c611d3f80c6774acb5f4
Yassine-Kial <kialmohamedyassine@gmail.com>
2023-12-20 16:52:24 +0100

    form1 done !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { PageLoginComponent } from "./pages/page-login/page-login.component";
 import { PageSignupComponent } from "./pages/page-signup/page-signup.component";
 import { PageResultComponent } from "./pages/page-result/page-result.component";
+import { BasicInformationComponent } from './pages/basic-information/basic-information.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, PageLoginComponent, PageSignupComponent, PageResultComponent , ]
+    imports: [CommonModule, RouterOutlet, PageLoginComponent, PageSignupComponent, PageResultComponent , BasicInformationComponent]
 })
 export class AppComponent {
   title = 'Medvisor';
