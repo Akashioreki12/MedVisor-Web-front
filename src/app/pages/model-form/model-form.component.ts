74c90cec07d3d42824bce697a28b61a40d50c068
Yassine-Kial <kialmohamedyassine@gmail.com>
2024-01-02 12:03:26 +0100

    form updated

--- src/app/pages/model-form/model-form.component.ts
+++ src/app/pages/model-form/model-form.component.ts
@@ -1,11 +1,12 @@
 import { Component, ViewChild } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule,Router } from '@angular/router';
 import { MatStepperModule, MatStepper } from '@angular/material/stepper';
 import { BasicInformationComponent } from '../basic-information/basic-information.component';
 import { AdditionalInformationComponent } from '../additional-information/additional-information.component';
 import { HealthInformationComponent } from '../health-information/health-information.component';
-import { CheckboxModule } from 'primeng/checkbox';
-
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import { CommonModule } from '@angular/common';
+import { FormDataService} from '../../form-data.service';
 @Component({
   selector: 'app-model-form',
   standalone: true,
@@ -14,27 +15,56 @@ import { CheckboxModule } from 'primeng/checkbox';
     BasicInformationComponent,
     AdditionalInformationComponent,
     HealthInformationComponent,
-  CheckboxModule],
+  CdkStepperModule,CommonModule],
   templateUrl: './model-form.component.html',
   styleUrl: './model-form.component.css'
 })
 export class ModelFormComponent { 
   @ViewChild('stepper') stepper!: MatStepper;
 
+  @ViewChild('basicInformationComponent') basicInformationComponent!: BasicInformationComponent;
+  @ViewChild('healthInformationComponent') healthInformationComponent!: HealthInformationComponent;
+  @ViewChild('additionalInformationComponent') additionalInformationComponent!: AdditionalInformationComponent;
+    formData: any = {};
 
+  constructor(private router: Router, private formDataService: FormDataService) {}
 
+ onNext(): void {
+    const activeStepIndex = this.stepper.selectedIndex;
 
-
-
-  goBack() {
-    if (this.stepper.selectedIndex > 0) {
-      this.stepper.previous();
+    switch (activeStepIndex) {
+      case 0:
+        this.formData.basicInformation = this.basicInformationComponent.onNext();
+        break;
+      case 1:
+        this.formData.healthInformation = this.healthInformationComponent.onNext();
+        break;
+      case 2:
+        this.formData.additionalInformation = this.additionalInformationComponent.onNext();
+        break;
     }
-  }
 
-  goForward() {
-    if (this.stepper.selectedIndex < this.stepper.steps.length - 1) {
+    if (activeStepIndex === this.stepper._steps.length - 1) {
+      this.logFormData();
+      this.router.navigate(['/page-result']);
+    } else {
+      // Move to the next step
       this.stepper.next();
     }
   }
+
+
+
+  logFormData(): void {
+
+    console.log('Basic Information:', this.formData.basicInformation);
+    console.log('Health Information:',  this.formData.healthInformation);
+    console.log('Additional Information:', this.formData.additionalInformation);
+  }
+
+
+  
+
+
+
 }
